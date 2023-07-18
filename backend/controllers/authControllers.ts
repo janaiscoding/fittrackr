8ba9233399fb20684928053a89af4da96e541f6d
janaiscoding <jana.istrate@gmail.com>
2023-07-18 14:04:59 +0300

    Add confirmation password logic

--- backend/controllers/authControllers.ts
+++ backend/controllers/authControllers.ts
@@ -64,6 +64,17 @@ const create_user = [
     .isLength({ min: 8, max: 24 })
     .withMessage("Password must be between 8 and 24 characters long.")
     .escape(),
+  body("confPassword")
+    .trim()
+    .exists()
+    .withMessage(
+      "Confirmation password must exist and match your other password."
+    )
+    .isLength({ min: 8, max: 24 })
+    .withMessage(
+      "Confirmation password must be between 8 and 24 characters long."
+    )
+    .escape(),
   async (req: Request, res: Response) => {
     const {
       age,
@@ -73,6 +84,7 @@ const create_user = [
       last_name,
       email,
       password,
+      confPassword,
     } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -80,38 +92,42 @@ const create_user = [
         errors: errors.array(),
       });
     }
-    try {
-      bcrypt.hash(password, 10, async (err, hashed) => {
-        if (err) {
-          res.status(500).json({ message: err.message });
-        } else {
-          await User.create({
-            age,
-            cur_weight,
-            goal_weight,
-            first_name,
-            last_name,
-            email, // unique in db
-            password: hashed,
-          })
-            .then(() => {
-              res
-                .status(201)
-                .json({ message: "New user successfully created!" });
+    if (password === confPassword) {
+      try {
+        bcrypt.hash(password, 10, async (err, hashed) => {
+          if (err) {
+            res.status(500).json({ message: err.message });
+          } else {
+            await User.create({
+              age,
+              cur_weight,
+              goal_weight,
+              first_name,
+              last_name,
+              email, // unique in db
+              password: hashed,
             })
-            .catch((err) => {
-              res.status(400).json({
-                message: "Email is already in use!",
-                error: err.message,
+              .then(() => {
+                res
+                  .status(201)
+                  .json({ message: "New user successfully created!" });
+              })
+              .catch((err) => {
+                res.status(400).json({
+                  message: "Email is already in use!",
+                  error: err.message,
+                });
               });
-            });
-        }
-      });
-    } catch (err: any) {
-      res.status(500).json({
-        message: "Internal server error",
-        error: err.message,
-      });
+          }
+        });
+      } catch (err: any) {
+        res.status(500).json({
+          message: "Internal server error",
+          error: err.message,
+        });
+      }
+    } else {
+      res.status(400).json({ message: "Passwords do not match." });
     }
   },
 ];
