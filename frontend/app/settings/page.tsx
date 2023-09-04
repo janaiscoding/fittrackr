5f4bada8b8c16a695e77e01de3d7ebf78a5ab891
janaiscoding <jana.istrate@gmail.com>
2023-09-04 20:50:33 +0300

    Add content form on settings page

--- frontend/app/settings/page.tsx
+++ frontend/app/settings/page.tsx
@@ -1,10 +1,11 @@
-'use client'
+"use client";
 import { useContext } from "react";
 import useTokenVerification from "../hooks/useTokenVerification";
 import { ModalContext } from "../context/modalContext";
 import TopNav from "../components/navigation/TopNav";
 import BotNav from "../components/navigation/BotNav";
 import FormModal from "../components/modals/FormModal";
+import Title from "../components/ui_elements/Title";
 
 const Settings = () => {
   useTokenVerification();
@@ -13,8 +14,21 @@ const Settings = () => {
   return (
     <div className="bg-black">
       <TopNav />
-      <div className="max-w-7xl m-auto min-h-[90vh] flex justify-between items-start gap-2 p-2">
-        settings page
+      <div className="max-w-7xl m-auto min-h-[90vh] flex justify-center items-start gap-2 p-2">
+        <div className="items-center justify-center">
+          <Title title={"Settings. Edit profile"} />
+          <form className="flex flex-col gap-2">
+            <label>
+              <p>First name</p>
+              <input></input>
+            </label>
+            <label>
+              <p>Last name</p>
+              <input></input>
+            </label>
+          </form>
+          <div>Delete account</div>
+        </div>
         {modalContext.modalPost && <FormModal />}
       </div>
       <BotNav />
